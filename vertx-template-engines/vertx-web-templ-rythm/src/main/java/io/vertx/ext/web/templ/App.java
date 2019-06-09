64090237f7aadeaecae24fbdf4eaa680e90edf9f
torq <victorqrsilva@gmail.com>
2019-06-09 15:09:06 -0300

    Add support for Rythm template engine

---
+++ vertx-template-engines/vertx-web-templ-rythm/src/main/java/io/vertx/ext/web/templ/App.java
@@ -0,0 +1,13 @@
+package io.vertx.ext.web.templ;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
